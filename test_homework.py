e4e25b186f80482f830c97c1e8c1874b950aff0a
pavelkalinchuk <715m4s@gmail.com>
2024-12-03 23:03:42 +0300

    Выполнен тест "test_readable_function".

--- test_homework.py
+++ test_homework.py
@@ -27,8 +27,6 @@ def test_dark_theme_by_time_and_user_choice():
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
 
-    is_dark_theme = None
-
     if dark_theme_enabled_by_user:
         is_dark_theme = True
     elif dark_theme_enabled_by_user is None:
@@ -98,15 +96,23 @@ def test_readable_function():
 
 
 def open_browser(browser_name):
-    actual_result = None
+    actual_result = print_function_name_and_args(open_browser, browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
+    actual_result = print_function_name_and_args(go_to_companyname_homepage, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
+    actual_result = print_function_name_and_args(find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
+
+
+def print_function_name_and_args(func, *args):  # функция принимает имя функции и значения аргументов
+    func_name = func.__name__.replace('_', ' ').title()  # получаем имя функции и заменяем подчеркивания на пробелы и
+    # преобразовываем в формат заголовка и записываем в переменную "func_name"
+    args_name = ", ".join([*args])  # объединяем произвольное количество аргументов в строку, разделяя их запятой и
+    # пробелом.
+    return f"{func_name} [{args_name}]"
